5bb5e90d5f57f7e49b495f70a80302f028e098ed
spirosmastorakis <spiros.mastorakis@gmail.com>
2015-10-15 15:26:13 -0700

    Skeleton for posting added

---
+++ partybnb/app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :name, :images, :description, :price, :availability, :created_at, :updated_at
---
+++ partybnb/spec/factories/posts.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :post do
+    name "MyString"
+images "MyString"
+description "MyText"
+price "9.99"
+availability "2015-10-15 15:24:21"
+  end
+
+end
