fd3536a7d35258c3a7fd1348026cf56dfac7cc7c
Mickey Sweatt <sweattm92@gmail.com>
2015-10-16 16:53:31 -0700

    Removed production code from secrets.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :transactions
   resources :posts
+  resources :post_images
   root to: 'visitors#index'
   devise_for :users
   resources :users
--- db/migrate/20151016053508_create_post_images.rb
+++ db/migrate/20151016053508_create_post_images.rb
@@ -3,7 +3,6 @@ class CreatePostImages < ActiveRecord::Migration
     create_table :post_images do |t|
       t.string :image_url
       t.integer :post_id
-
       t.timestamps null: false
     end
   end
