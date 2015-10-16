bdf704519ed4ebe828bccc31117cee994d8c344f
Mickey Sweatt <sweattm92@gmail.com>
2015-10-16 14:03:35 -0700

    Transistioned database to mysql

--- db/schema.rb
+++ db/schema.rb
@@ -14,49 +14,49 @@
 ActiveRecord::Schema.define(version: 20151016054122) do
 
   create_table "post_images", force: :cascade do |t|
-    t.string   "image_url"
-    t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "image_url",  limit: 255
+    t.integer  "post_id",    limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.decimal  "price"
+    t.string   "name",         limit: 255
+    t.text     "description",  limit: 65535
+    t.decimal  "price",                      precision: 10
     t.datetime "availability"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.string   "creditCardNumber"
-    t.string   "cardholderName"
-    t.string   "type"
-    t.string   "securityPass"
+    t.string   "creditCardNumber", limit: 255
+    t.string   "cardholderName",   limit: 255
+    t.string   "type",             limit: 255
+    t.string   "securityPass",     limit: 255
     t.date     "expirationDate"
-    t.decimal  "price"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.decimal  "price",                        precision: 10
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
+    t.string   "name",                   limit: 255
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
