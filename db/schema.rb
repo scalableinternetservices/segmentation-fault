79d79b8b584021730bba9012fb67484fd383dff9
spirosmastorakis <spiros.mastorakis@gmail.com>
2015-10-21 19:18:20 -0700

    Adding link to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,26 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151018041256) do
+ActiveRecord::Schema.define(version: 20151022044901) do
 
   create_table "post_images", force: :cascade do |t|
-    t.string   "image_url",  limit: 255
-    t.integer  "post_id",    limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "name",               limit: 255
-    t.text     "description",        limit: 65535
-    t.decimal  "price",                            precision: 10
-    t.datetime "availability"
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.string   "image_url",          limit: 255
+    t.integer  "post_id",            limit: 4
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.string   "image_file_name",    limit: 255
     t.string   "image_content_type", limit: 255
     t.integer  "image_file_size",    limit: 4
     t.datetime "image_updated_at"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "name",         limit: 255
+    t.text     "description",  limit: 65535
+    t.decimal  "price",                      precision: 10
+    t.datetime "availability"
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
+    t.string   "categories",   limit: 255
+    t.text     "restrictions", limit: 65535
   end
 
   create_table "transactions", force: :cascade do |t|
