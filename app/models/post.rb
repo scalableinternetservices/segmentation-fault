4a0b91b5233fb44f3c3ae0791553a044eac6c884
Mickey Sweatt <sweattm92@gmail.com>
2015-10-23 22:03:52 -0700

    Added attached images to show view

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,7 @@
 class Post < ActiveRecord::Base
   validates :name, :description, :price, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  has_attached_file :image
   has_many :post_images
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
 
 # Name	Images	Description	Price	Availability
--- db/migrate/20151024045627_remove_image_attachments_from_posts.rb
+++ db/migrate/20151024045627_remove_image_attachments_from_posts.rb
@@ -1,6 +1,6 @@
 require_relative '20151018041256_add_image_attachments_to_posts'
 
-class RemoveImageAttachmentsFromPostImages < ActiveRecord::Migration
+class RemoveImageAttachmentsFromPosts < ActiveRecord::Migration
   def change
     revert AddImageAttachmentsToPosts
   end
