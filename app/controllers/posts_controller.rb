3a0b43be12c9d52b01bad1e4fc1609f0be5a1a9f
Rishi Mody <rishi.mody@hotmail.com>
2015-10-29 16:07:29 -0700

    Validate user logged in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:create]
 
   # GET /posts
   # GET /posts.json
@@ -25,6 +26,13 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
+
+    # if !current_user
+    #   respond_to do |format|
+    #     format.html { render :new }
+    #     format.json { render json: @post.errors, status: :unprocessable_entity }
+    #   end
+    # end
 
     @post = Post.new(post_params)
 
