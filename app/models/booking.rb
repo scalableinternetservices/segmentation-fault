7b3345ca67b63f51fe9e6f0579c634ae5da2a0e9
Mickey Sweatt <sweattm92@gmail.com>
2015-10-30 15:31:56 -0700

    Completed implemenation of 'Book' button

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ActiveRecord::Base
   belongs_to :user, :class_name => 'User'
-  belongs_to :money, foreign_key: "transaction_id", :class_name => 'Transaction'
+  has_one :money, foreign_key: "transaction_id", :class_name => 'Transaction'
   belongs_to :post, :class_name => 'Post'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :bookings
   resources :transactions
   resources :posts
-  get "posts/book/:id" => "posts#book"
+  post "posts/book/" => "posts#book"
   resources :post_images
   root to: 'visitors#index'
   devise_for :users
