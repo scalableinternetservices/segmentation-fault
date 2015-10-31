0a744e085fa1d15a568089cdf8e7cf3192849af5
Mickey Sweatt <sweattm92@gmail.com>
2015-10-30 17:49:10 -0700

    Added and used routing helper for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   resources :post_images
   root to: 'visitors#index'
   devise_for :users
-  get "users/profile/" => "users#profile"
+  get "users/profile/" => "users#profile" , as: 'user_profile'
   resources :users
 end
