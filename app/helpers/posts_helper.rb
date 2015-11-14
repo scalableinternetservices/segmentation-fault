6b5777f6c92d7e8b76d3567a81e4868f406dc946
spirosmastorakis <spiros.mastorakis@gmail.com>
2015-11-13 17:05:40 -0800

    Adding server side caching on posts and post images

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,5 @@
 module PostsHelper
+  def cache_key_for_post_row(post)
+    "post-#{post.id}-#{post.name}-#{post.description}-#{post.price}-#{post.availability}-#{post.restrictions}-#{post.categories}"
+  end
 end
