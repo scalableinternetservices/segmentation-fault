bd844942bf5b2a99be2ad1d26bfa872b76fe6e32
Mickey Sweatt <sweattm92@gmail.com>
2015-11-24 23:32:44 -0800

    Preventing the seed file from running more than once

---
+++ db/migrate/20151125072822_create_seed_mutex.rb
@@ -0,0 +1,9 @@
+class CreateSeedMutex < ActiveRecord::Migration
+  def change
+    create_table :seed_mutexes do |t|
+      t.boolean :acquired
+      t.index :acquired, :unique => true
+      t.timestamps null: false
+    end
+  end
+end
